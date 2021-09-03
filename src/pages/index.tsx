dc7690327ed814ac68255c0b984bb5dcf78f5b01
Eduardo Santos <eduardosantos.ist@gmail.com>
2021-09-03 17:33:43 -0300

    Fundamentos do Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,60 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
+import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+    recurringInterval: string;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>World</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>
+            News about the <span>React</span> world
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>
+              for {product.amount}/{product.recurringInterval}
+            </span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JViQ7JUzhFkiU8NFoTF44UQ");
+
+  const product = {
+    priceId: price.id,
+    recurringInterval: price.recurring.interval,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(
+      price.unit_amount / 100 // api retorna em centavos
+    ),
+  };
+
+  return {
+    props: { product },
+    revalidate: 60 * 60 * 24, // 24 horas
+  };
+};
